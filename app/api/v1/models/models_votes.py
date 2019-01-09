2272d76e753510b1d9ccf643d8bcb3b6304f6805
wathigo <wathigosimon@gmail.com>
2019-01-09 18:04:07 -0500

    [feature #163048360] patch endpoint for the upvote implemented

---
+++ app/api/v1/models/models_votes.py
@@ -0,0 +1,40 @@
+import datetime
+
+votes_records = []
+
+class VotesRecord():
+    def __init__(self):
+        self.rec = votes_records
+
+    def find(self, id):
+        response = None
+        for item in self.rec:
+            if item['id'] == id:
+                response = item
+        return response
+
+    def save(self, id, vote):
+        ### True arguement is for an Upvote
+        data = {
+            "question_id" : id,
+            "upvotes" : 0,
+            "downvotes" : 0,
+            "voted_on" : datetime.datetime.now()
+        }
+        ### True arguement is for an Upvote
+        if vote:
+            data['upvotes'] = data['upvotes'] + 1
+            ### False arguement is for an downvote
+        else:
+            data['downvotes'] = data['downvotes'] + 1
+        self.rec.append(data)
+        return self.rec
+
+    def upvote(self, id):
+        record = self.find(id)
+        if record is not None:  ### Record exists...
+            record['upvotes'] = record['upvotes'] + 1
+            return record
+        else: ### record does not exists
+            response = self.save(id, True)
+        return response
---
+++ app/api/v1/views/views_votes.py
@@ -0,0 +1,13 @@
+from flask import jsonify, make_response, request
+from flask_restful import Resource
+from app.api.v1 import version_one as v1
+
+from ..models.models_votes import VotesRecord
+
+class Upvotes(VotesRecord, Resource):
+    def __init__(self):
+        self.rec = VotesRecord()
+
+    def patch(self, id):
+        response = self.rec.upvote(id)
+        return make_response(jsonify({"My updated votes records are": response}), 201)
