111ed493121961b0d972849764cf3fae82e57621
wathigo <wathigosimon@gmail.com>
2019-01-09 08:37:49 -0500

    [feature #163048239] Add a failing test

--- app/tests/v1/test_meetups.py
+++ app/tests/v1/test_meetups.py
@@ -14,15 +14,23 @@ class TestMeetups(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client()
 
-    ###Test meetups creation
-    def test_01_post(self):
+    def create_record(self):
         response = self.client.post('/api/v1/meetups', \
             data=json.dumps({
                 "Title": "Gaming",
                 "Description": "There will be a meetup for gamers on 25th of August..."
                 }),\
             headers={"content-type": "application/json"})
+        return response
+
+    ###Test meetups creation
+    def test_01_post(self):
+        response = self.create_record()
         self.assertEqual(response.status_code, 201)
 
     def test_02_get(self):
         response = self.client.get('/api/v1/meetups', headers={"content-type": "application/json"})
+
+    def test_03_get(self):
+        self.create_record()
+        response = self.client.get('/api/v1/meetups/1', headers={"content-type": "application/json"})
