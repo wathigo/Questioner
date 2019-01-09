7895ab3d6234264e4558f9fba7d464f246efe4a1
wathigo <wathigosimon@gmail.com>
2019-01-09 14:25:41 -0500

    [finishes #163048108] Add route to post question endpoint

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -5,5 +5,7 @@ version_one = Blueprint('api_v1', __name__, url_prefix='/api/v1')
 api = Api(version_one)
 
 from .views.views_meetups import Meetup, Meetups
+from .views.views_questions import Questions
 api.add_resource(Meetup, '/meetups')
 api.add_resource(Meetups, '/meetups/<int:id>')
+api.add_resource(Questions, '/meetups/<int:id>/questions')
