8422eeacf3c5ccb487bccb4ed29ce458e0651eb8
wathigo <wathigosimon@gmail.com>
2019-01-11 00:33:01 -0500

    [starts #163048510] Add a failing test

---
+++ app/tests/v1/test_reserve.py
@@ -0,0 +1,27 @@
+""" import the necessary modules """
+
+import unittest
+import json
+from app import create_app
+from run import appl
+
+
+""" class to test reserve endpoint """
+class TestQuestions(unittest.TestCase):
+
+    def setUp(self):
+        appl.testing = True
+        self.app = create_app()
+        self.client = self.app.test_client()
+
+    def create_record(self):
+        response = self.client.post('/api/v1/meetups/1/rsvps', \
+            data=json.dumps({
+                "reserve" : "yes"
+                }),\
+            headers={"content-type": "application/json"})
+        return response
+
+    def test_01_post(self):
+        response = self.create_record()
+        self.assertEqual(response.status_code, 201)
