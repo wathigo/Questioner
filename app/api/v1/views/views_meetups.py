c47d0963de52778feda193f25d71b8a87c12e706
wathigo <wathigosimon@gmail.com>
2019-01-11 02:53:52 -0500

    [starts #163141939] Remove unnecessary import

--- app/api/v1/views/views_meetups.py
+++ app/api/v1/views/views_meetups.py
@@ -1,6 +1,5 @@
 from flask import jsonify, make_response, request
 from flask_restful import Resource
-from app.api.v1 import version_one as v1
 from ..models.models_meetup import MeetupRecord
 
 class Meetup(MeetupRecord, Resource):
