74d9d3659e9a2f966d531705771c75795b1fd60b
wathigo <wathigosimon@gmail.com>
2019-01-11 17:43:31 -0500

    [starts #163162731] Fix pylint warning

--- app/api/v1/models/models_question.py
+++ app/api/v1/models/models_question.py
@@ -6,11 +6,11 @@ class QuestionRecord():
     def __init__(self):
         self.rec = question_rec
 
-    def save(self, question, id):
+    def save(self, meetup_id, question):
         data = {
             "createdOn": datetime.datetime.now(),
             "id" : len(question_rec) + 1,
-            "meetupId" : id,
+            "meetupId" : meetup_id,
             "question" : question
         }
         self.rec.append(data)
