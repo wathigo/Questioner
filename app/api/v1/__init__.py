d20c733280d52b9cd044570fd2be2ba76c4d8e36
wathigo <wathigosimon@gmail.com>
2019-01-11 00:34:10 -0500

    [feature #163048510] Add the route to post reserve

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -7,8 +7,10 @@ api = Api(version_one)
 from .views.views_meetups import Meetup, Meetups
 from .views.views_questions import Questions
 from .views.views_votes import Upvotes, Downvotes
+from .views.views_reserve import Reserve
 api.add_resource(Meetup, '/meetups')
 api.add_resource(Meetups, '/meetups/<int:id>')
+api.add_resource(Reserve, '/meetups/<int:id>/rsvps')
 api.add_resource(Questions, '/meetups/<int:id>/questions')
 api.add_resource(Upvotes, '/questions/<int:id>/upvote')
 api.add_resource(Downvotes, '/questions/<int:id>/downvote')
