d9789ddf76ded2d20886d3461c966765d3d6cf53
wathigo <wathigosimon@gmail.com>
2019-01-11 04:00:09 -0500

    [finishes #163141939] PUT request for the meetup record implemented

--- app/api/v1/views/views_meetups.py
+++ app/api/v1/views/views_meetups.py
@@ -1,30 +1,49 @@
+""" import the necessary modules """
 from flask import jsonify, make_response, request
 from flask_restful import Resource
 from ..models.models_meetup import MeetupRecord
 
 class Meetup(MeetupRecord, Resource):
+    """ enpoints to route without a meetup id """
     def __init__(self):
         self.rec = MeetupRecord()
 
     def post(self):
+        """ post endpoint for meetup record craetion """
         data = request.get_json()
         title = data['Title']
         description = data['Description']
-        responce = self.rec.save(title, description)
+        date = data['Date']
+        location = data['Location']
+        responce = self.rec.save(title, description, date, location)
         return make_response(jsonify({"My new meetup records are": responce}), 201)
 
     def get(self):
+        """ get endpoint to get all the records """
         data = self.rec.get_items()
         return make_response(jsonify({"My meetup records are": data}), 200)
 
 
 class Meetups(MeetupRecord, Resource):
+    """ enpoints to route with a meetup id """
     def __init__(self):
         self.rec = MeetupRecord()
 
     def get(self, id):
+        """ get endpoint to get a specific meetup record """
         item = self.rec.get_item(id)
-        if item is not None:
+        if item is not None: ### item found
             return make_response(jsonify({"My meetup record is": item}), 200)
         else:
-            return make_response(jsonify({"Error": "Item not found!"}))
+            return make_response(jsonify({"Message": "Item not found!"}))
+
+    def put(self, id):
+        data = request.get_json()
+        item = self.rec.get_item(id)
+        if item is None: ### item not found
+            return make_response(jsonify({"Message": "Item not found!"}), 204)
+        item['Title'] = data['Title']
+        item['Description'] = data['Description']
+        item['Date'] = data['Date']
+        item['Location'] = data['Location']
+        return make_response(jsonify({"Record updated successfully": item}), 200)
