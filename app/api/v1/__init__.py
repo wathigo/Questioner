dae5fb3e8b67cd8a20bfbf973d6c74fb0672117a
wathigo <wathigosimon@gmail.com>
2019-01-13 15:41:01 -0500

    [feature #163178207] Add route to register user

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,13 +1,16 @@
+""" import the necessary modules"""
 from flask_restful import Api
 from flask import Blueprint
 
 version_one = Blueprint('api_v1', __name__, url_prefix='/api/v1')
 api = Api(version_one)
-
 from .views.views_meetups import Meetup, Meetups
 from .views.views_questions import Questions
 from .views.views_votes import Upvotes, Downvotes
 from .views.views_reserve import Reserve
+from .views.views_user import Users
+
+api.add_resource(Users, '/auth/signup')
 api.add_resource(Meetup, '/meetups')
 api.add_resource(Meetups, '/meetups/<int:id>')
 api.add_resource(Reserve, '/meetups/<int:id>/rsvps')
