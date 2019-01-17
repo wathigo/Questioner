e343bdc2a541b0836273707dc62c53d7e45698c0
wathigo <wathigosimon@gmail.com>
2019-01-17 00:35:16 -0500

    [Chore #163259823] create tables implemented during app initialization

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,16 @@
 """ Import the necessary modules """
+import os
 from flask import Flask, Blueprint
 from instance.config import app_config
-
+from .utils.database import create_tables
 from .api.v1 import version_one as v1
 
 def create_app():
     """ Create the app """
     app = Flask(__name__)
     app.register_blueprint(v1)
-    app.config.from_object(app_config)
+    config_env = os.environ.get('FLASK_ENV')
+    app.config.from_object(app_config[config_env])
+    create_tables()
+
     return app
