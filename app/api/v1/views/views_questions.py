fb0fe09c8f268775215214f39573a20e4972d66c
wathigo <wathigosimon@gmail.com>
2019-01-18 18:22:19 -0500

    [Chore #163332710] Code for question record endpoints refactored

--- app/api/v1/views/views_questions.py
+++ app/api/v1/views/views_questions.py
@@ -1,26 +1,54 @@
+""" import the necessary modules """
 from flask import jsonify, make_response, request
 from flask_restful import Resource
-from app.api.v1 import version_one as v1
-from ..models.models_meetup import MeetupRecord
 from ....utils.validators import Views
 
 from ..models.models_question import QuestionRecord
 
 class Questions(QuestionRecord, Resource):
+    """ Class to implement question endpont """
     def __init__(self):
         self.question_models = QuestionRecord()
         self.validate = Views()
 
     def post(self, id):
+        """ post question request endpoint implementation"""
         data = request.get_json()
         valid = self.validate.validate_question_keys(data)
         if valid:
             return make_response(jsonify({"status" : 400,
                                           "Error": valid}), 400)
         question = data['question']
-        response = self.question_models.save(id, question)
+        response = self.question_models.create_record(id, question)
         if response is not None:
             return make_response(jsonify({"status" : 201,
                                           "data": response}), 201)
         return make_response(jsonify({"status" : 500,
                                       "Error": "Record could not be created"}), 500)
+
+
+class Upvotes(QuestionRecord, Resource):
+    """ Upvote a question endpoint """
+    def __init__(self):
+        self.models = QuestionRecord()
+
+    def patch(self, id):
+        """ upvote question endpoint implementation """
+        response = self.models.vote(id, True)
+        if not response:
+            return make_response(jsonify({"status" : 404,
+                                          "Error": "Question record not found!"}), 201)
+        return make_response(jsonify({"status" : 201,
+                                      "data": response}), 201)
+
+
+class Downvotes(Upvotes):
+    """ downvote a question record """
+    def __init__(self):
+        super(Downvotes, self).__init__()
+
+    def patch(self, id):
+        """ downvote question endpoint implementation """
+        response = self.models.vote(id, False)
+        return make_response(jsonify({"status" : 201,
+                                      "data": response}), 201)
