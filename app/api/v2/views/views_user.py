d54b7f6670754f68e660794b7c2fd1bb7d45fb5a
wathigo <wathigosimon@gmail.com>
2019-01-21 18:37:30 -0500

    [Chore #163348667] User views updated to include a post login endpoint

--- app/api/v2/views/views_user.py
+++ app/api/v2/views/views_user.py
@@ -18,6 +18,10 @@ class Users(UserRecord, Resource):
             return make_response(jsonify({"status" : 400,
                                           "Error": errors}), 400)
         response = self.models.create_user(json_data)
+        if not response:
+            return make_response(jsonify({"status" : 400,
+                                          "Message": "A user with the given Email exists"}), 201)
+
         return make_response(jsonify({"status" : 201,
                                       "data": response}), 201)
 
@@ -25,7 +29,16 @@ class UserLogin(UserRecord, Resource):
     """ Class to authenticate a user"""
     def __init__(self):
         self.models = UserRecord()
+
     def post(self):
         """ endpoint for user login"""
         json_data = request.get_json()
         response = self.models.authenticate_user(json_data)
+        if response is None:
+            return make_response(jsonify({"status" : 404,
+                                          "Error": "User does not exists!"}), 404)
+        elif response is False:
+            return make_response(jsonify({"status" : 400,
+                                          "Error": "Wrong username/password!"}), 400)
+        return make_response(jsonify({"status" : 200,
+                                      "Message": "Logged in successfully"}), 200)
