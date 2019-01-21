0705c2c4004eb64f05bc4ad72f392dd30375021e
wathigo <wathigosimon@gmail.com>
2019-01-21 13:46:19 -0500

    [Chore #163348667] Base models modified to use a database

--- app/api/v2/models/models_base.py
+++ app/api/v2/models/models_base.py
@@ -10,40 +10,43 @@ class BaseModels():
 
 
     def check_exists(self, key, value):
-        """ Check if an item exists in the database """
-        record = self.check_record_type()
-        items = [item for item in record if item[key] == value]
-        return len(items)
+        ''' Check if an item exists in the database '''
+        query = """ SELECT EXISTS (SELECT * FROM {} WHERE {}='{}'"""\
+                    .format(self.table_name, key, value)
+        cur = self.connection.cursor()
+        cur.execute(query)
+        found = cur.fetchone()
+        return found
 
     def find(self, key, value):
-        """ Find an item with a specific key and value"""
-        items = False
-        record = self.check_record_type()
-        item = [item for item in record if item[key] == value]
-        if item:
-            items = item
-        return items
+        ''' Find an item with a specific key and value'''
+        query = """ SELECT * FROM {} WHERE {}='{}'""".format(self.table_name, key, value)
+        cur = self.connection.cursor()
+        cur.execute(query)
+        data = cur.fetchone()
+        return data
 
     def return_record(self):
-        """ Return the whole data structure"""
-        record = self.check_record_type()
-        return record
+        ''' Return the whole database'''
+        query = """ SELECT * FROM {}""".format(self.table_name)
+        cur = self.connection.cursor()
+        cur.execute(query)
+        data = cur.fetchall()
+        return data
 
     def save(self, query, data):
-        """ Save data to a database"""
+        ''' Save data to a database'''
         save = self.connection
         cur = save.cursor()
         cur.execute(query)
         save.commit()
         return data
 
-    def check_record_size(self):
-        """ Check and return the size of the record """
-        record = self.check_record_type()
-        return len(record)
 
     def delete(self, key, value):
-        """ Function to delete item """
-        item = self.find(key, value)
-        record = self.check_record_type()
-        record.remove(item)
+        '''Function to delete of an a row item'''
+        data = self.check_exists(key, value)
+        if data:
+            query = """ DELETE FROM {} WHERE {}='{}'""".format(self.table_name, key, value)
+            cur = self.connection.cursor()
+            cur.execute(query)
