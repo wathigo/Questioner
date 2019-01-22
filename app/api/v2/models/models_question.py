3f5512427bc338706a956a00c64c57e719790c86
wathigo <wathigosimon@gmail.com>
2019-01-22 04:05:10 -0500

    [Chore #163348667] question models modified

--- app/api/v2/models/models_question.py
+++ app/api/v2/models/models_question.py
@@ -21,31 +21,33 @@ class QuestionRecord(BaseModels):
         self.record.save(query, data)
         return data
 
-    def update_column_value(self, primary_key, value):
+    def update_column_value(self, primary_key, vote):
         """ Update votes column method """
-        query = """ UPDATE question SET votes={} WHERE questionid={}""".format(value, primary_key)
+        if vote:
+            query = """ UPDATE question SET votes=votes+1 WHERE questionid={}""".format(primary_key)
+        else:
+            query = """ UPDATE question SET votes=votes-1 WHERE questionid={}""".format(primary_key)
         cur = self.record.connection.cursor()
         cur.execute(query)
         return self.record.find('questionid', primary_key)
+
+    def modify_response(self, data):
+        """ Method to modify question record response(turple->dict) """
+        response = {
+            "Title" : data[1],
+            "Question" : data[2],
+            "Votes" : data[3]
+            }
+        return response
 
     def vote(self, question_id, vote):
         ''' method to upvote/downvote a question '''
         data = False
         item = self.record.find('questionid', question_id)
         if item:
-            found = item[3]
-            if vote: ## Upvotes
-                if found is None:
-                    found = 0
-                    found = found + 1
-                    data = self.update_column_value(question_id, found)
-                else:
-                    found = found + 1
-                    data = self.update_column_value(question_id, found)
+            if not vote and item[3] < 1:
+                data = self.modify_response(item)
             else:
-                if found > 0 and found is not None:
-                    found = found - 1
-                    data = self.update_column_value(question_id, found)
-                else:
-                    data = self.record.find('questionid', question_id)
+                record = self.update_column_value(question_id, vote)
+                data = self.modify_response(record)
         return data
