0232d4b5353c5359c6528cc6f2a2095f65d051fd
wathigo <wathigosimon@gmail.com>
2019-01-21 19:35:18 -0500

    [Chore #163348667] Models meetup modified

--- app/api/v2/models/models_meetup.py
+++ app/api/v2/models/models_meetup.py
@@ -1,33 +1,33 @@
-""" import datetime module """
-import datetime
+""" local imports """
 from .models_base import BaseModels
 
 class MeetupRecord(BaseModels):
     """ Constructor that calls the Constructor in BaseModels"""
     def __init__(self):
-        self.records = BaseModels('meetup')
+        self.records = BaseModels('meetups')
 
     def create_record(self, data):
         """ create a new meetup record """
         data = {
-            "createdOn" : datetime.datetime.now(),
-            "id" : self.records.check_record_size()+1,
             "Title": data['Title'],
             "Description": data['Description'],
             "Date" : data['Date'],
             "Location" : data['Location']
             }
-        record = self.records.save(data)
+        query = """INSERT INTO meetups(title, description, location, happeningon)
+        VALUES ('%s', '%s', '%s', '%s');""" % \
+        (data['Title'], data['Description'], data['Location'], data['Date'])
+        record = self.records.save(query, data)
         return record
 
     def get_items(self):
-        """ Return the whole data structure"""
+        """ Return the whole database"""
         return self.records.return_record()
 
     def get_item(self, item_id):
         """ get a specific item """
         found = False
-        item = self.records.find('id', item_id)
+        item = self.records.find('meetupid', item_id)
         if item:
             found = item
         return found
