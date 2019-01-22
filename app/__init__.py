71dde92f234f1774d81d7e131446e00d3d2ab348
wathigo <wathigosimon@gmail.com>
2019-01-22 07:29:02 -0500

    [Chore #163348667] Json web authorization implemented in the user endpoints

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 """ Import the necessary modules """
 import os
+from flask_jwt_extended import JWTManager
 from flask import Flask, Blueprint
 from instance.config import app_config
 from .utils.database import create_tables
@@ -14,6 +15,8 @@ def create_app():
     app.register_blueprint(v2)
     config_env = os.environ.get('FLASK_ENV')
     app.config.from_object(app_config[config_env])
+    app.config['JWT_SECRET_KEY'] = 'secret-string'
+    jwt = JWTManager(app)
     create_tables()
 
     return app
--- app/api/v2/views/views_user.py
+++ app/api/v2/views/views_user.py
@@ -1,6 +1,7 @@
 """ import the necessary modules """
 from flask import jsonify, make_response, request
 from flask_restful import Resource
+from flask_jwt_extended import create_access_token, create_refresh_token
 from ..models.models_user import UserRecord
 from ....utils.validators_schema import UserValidate
 
@@ -21,8 +22,11 @@ class Users(UserRecord, Resource):
         if not response:
             return make_response(jsonify({"status" : 400,
                                           "Message": "A user with the given Email exists"}), 400)
-
+        access_token = create_access_token(identity=response['Email'])
         return make_response(jsonify({"status" : 201,
+                                      "message" : "Logged in as{}".\
+                                      format(response['Email'].split("@")[0]),
+                                      "Access token" : access_token,
                                       "data": response}), 201)
 
 class UserLogin(UserRecord, Resource):
@@ -40,5 +44,8 @@ class UserLogin(UserRecord, Resource):
         elif response is False:
             return make_response(jsonify({"status" : 400,
                                           "Error": "Wrong username/password!"}), 400)
+        access_token = create_access_token(identity=json_data['Email'])
         return make_response(jsonify({"status" : 200,
-                                      "Message": "Logged in successfully"}), 200)
+                                      "Message": "Logged in as {}"\
+                                      .format(json_data['Email'].split("@")[0]),
+                                      "Access token" : access_token}), 200)
