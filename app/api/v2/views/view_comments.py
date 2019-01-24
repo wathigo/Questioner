f5aff303d384d3779ed474a23774ba1a45feac9d
wathigo <wathigosimon@gmail.com>
2019-01-24 05:52:45 -0500

    [Feature #163448647] POST comment endpoint implemented

---
+++ app/api/v2/views/view_comments.py
@@ -0,0 +1,26 @@
+""" import third party modules """
+from flask import jsonify, make_response, request
+from flask_restful import Resource
+from flask_jwt_extended import jwt_required, get_jwt_identity
+""" local imports"""
+from ..models.models_comments import CommentsRecord
+from ....utils.validators_schema import CommentValidate
+
+
+class Comment(CommentsRecord, Resource):
+    def __init__(self):
+        self.models = CommentsRecord()
+
+    @jwt_required
+    def post(self, id):
+        """ post comment endpoint implementation"""
+        json_data = request.get_json()
+        data, errors = CommentValidate().load(json_data)
+        if errors:
+            return make_response(jsonify({"status" : 400,
+                                          "Error": errors}), 400)
+
+        email = get_jwt_identity()
+        response = self.models.create_comment_record(json_data, id, email)
+        return make_response(jsonify({"status" : 201,
+                                      "data": response}), 201)
