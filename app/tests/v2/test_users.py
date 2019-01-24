a46f69290f083799d6029a207433ec5ac79f1af5
wathigo <wathigosimon@gmail.com>
2019-01-23 21:49:59 -0500

    [Chore #163348667] user tests modified

--- app/tests/v2/test_users.py
+++ app/tests/v2/test_users.py
@@ -3,34 +3,40 @@ import json
 from . import BaseTests
 
 
+TOKEN = None
 class TestUser(BaseTests):
     """ class to test users"""
     def create_record(self):
         """ Create a new user record for testing """
-        response = self.client.post('/api/v2/auth/signup', \
+        response = self.client.post('/api/v2/auth/admin/signup', \
             data=json.dumps({
                 "FirstName": "David",
                 "LastName": "Momanyi",
                 "Email" : "momanyidavid@gmail.com",
+                "OtherName" : "",
+                "PhoneNumber" : "",
                 "Password" : "bill_Bond23",
                 "RepeatPassword" : "bill_Bond23"
                 }),\
             headers={"content-type": "application/json"})
-        return response
+        response_data = response.json
+        return response_data
 
     ###Test user account creation
     def test_01_post(self):
         """ Test user record creation"""
         response = self.create_record()
-        self.assertEqual(response.status_code, 201)
+        print(response)
+        self.assertEqual(response['status'], 201)
 
-    """def test_02_post(self):
-        Test for user login
-        self.create_record()
+    def test_02_post(self):
+        """Test for user login"""
+        data = self.create_record()
         response = self.client.post('/api/v2/auth/login', \
             data=json.dumps({
                 "Email" : "momanyidavid@gmail.com",
                 "Password" : "bill_Bond23",
                 }),\
-            headers={"content-type": "application/json"})
-        self.assertEqual(response.status_code, 200)"""
+            headers={"content-type": "application/json",
+                     "Authorization" : "Bearer "+ data['access_token']})
+        self.assertEqual(response.status, '200 OK')
