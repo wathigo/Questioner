0872d96c778ffe2f6cbec11f6d140d673bc72322
wathigo <wathigosimon@gmail.com>
2019-01-24 22:10:34 -0500

    [Finishes #163464252] user tests updated

--- app/tests/v2/test_users.py
+++ app/tests/v2/test_users.py
@@ -27,7 +27,7 @@ class TestUser(BaseTests):
         """ Test user record creation"""
         response = self.create_record()
         print(response)
-        self.assertEqual(response['status'], 400)
+        self.assertEqual(response['status'], 201)
 
     def test_02_post(self):
         """Test for user login"""
