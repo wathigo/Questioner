395804fe5374d480532606d57e1b9f883e29054a
wathigo <wathigosimon@gmail.com>
2019-02-04 05:51:33 -0500

    Cor package included in requirements file

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 """ Import the necessary modules """
 import os
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 from flask import Flask, Blueprint
 from instance.config import app_config
 from .utils.database import create_tables, insert_data
@@ -10,11 +11,12 @@ from .api.v2 import version_two as v2
 def create_app():
     """ Create the app """
     app = Flask(__name__)
+    CORS(app)
     app.register_blueprint(v2)
     os.environ['FLASK_ENV'] = 'development'
     app.config.from_object(app_config['development'])
     app.config['JWT_SECRET_KEY'] = 'secret-string'
-    jwt = JWTManager(app)
+    JWTManager(app)
     create_tables()
     insert_data()
 
