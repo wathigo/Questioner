f95ebf4c1fa00b88a47134873f05e7755c3b94b3
wathigo <wathigosimon@gmail.com>
2019-02-04 09:51:17 -0500

    [Feature #163686363] index.js modified

--- UI/js/index.js
+++ UI/js/index.js
@@ -1,9 +1,9 @@
-
+document.getElementById('signup_form').addEventListener('submit', signUpUser);
 function signUpUser(event) {
     /*
     Function to perform user signup
     */
-
+    event.preventDefault();
     let data = {};
     let fieldnames = [
       'firstname',
@@ -14,26 +14,58 @@ function signUpUser(event) {
       'password',
       'repeatpassword'
     ];
-    let signupUrl = 'http://0.0.0.0:5000/api/v2/auth/admin/signup';
-
+    let signupUrl = 'https://questioner-api-048.herokuapp.com/api/v2/auth/signup';
+    console.log("successful");
     for (let name of fieldnames) {
             data[name] = document.getElementById(name).value;
         }
-
+    console.log(data)
     fetch(signupUrl, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: data
+        body: JSON.stringify(data)
     })
         .then((response) => response.json())
         .then((data => {
             if (data.status === 201){
                 console.log('Success:', JSON.stringify(data));
+                openForm('login')
             }
             else{
                 console.log(data.message);
             }
         }))
+        .catch((err)=>console.log(err));
+
+
+}
+function openForm(form_id){
+  if (form_id == 'register'){
+    document.getElementById('login').style.display='none';
+    document.getElementById('Admnlogin').style.display='none';
+  }
+  else if(form_id == 'login') {
+    document.getElementById('register').style.display='none';
+    document.getElementById('Admnlogin').style.display='none';
+  }
+  else{
+    document.getElementById('login').style.display='none';
+    document.getElementById('register').style.display='none';
+  }
+  var i, body_content;
+  body_content = document.getElementsByClassName("body_content");
+  for (i = 0; i < body_content.length; i++){
+    body_content[i].style.display = "none";
+  }
+  document.getElementById(form_id).style.display='block'
+}
+function closeForm(form_id){
+  document.getElementById(form_id).style.display='none'
+  var i, body_content;
+  body_content = document.getElementsByClassName("body_content");
+  for (i = 0; i < body_content.length; i++){
+    body_content[i].style.display = "block";
+  }
 }
